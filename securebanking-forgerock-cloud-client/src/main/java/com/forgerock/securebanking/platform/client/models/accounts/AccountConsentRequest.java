3488159459c9be6794612bcfb41e290d09d5f3fa
Andra <andra.racovita@itsmartsystems.eu>
2022-03-16 15:04:05 +0200

    Change license headers

--- securebanking-forgerock-cloud-client/src/main/java/com/forgerock/securebanking/platform/client/models/accounts/AccountConsentRequest.java
+++ securebanking-forgerock-cloud-client/src/main/java/com/forgerock/securebanking/platform/client/models/accounts/AccountConsentRequest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2020-2021 ForgeRock AS (obst@forgerock.com)
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,5 +33,9 @@ public class AccountConsentRequest extends ConsentRequest {
     public AccountConsentRequest(String intentId, SignedJWT consentRequestJwt, User user, String clientId, List<FRAccountWithBalance> accounts) {
         super(intentId, consentRequestJwt, user, clientId);
         this.accounts = accounts;
+    }
+
+    public List<FRAccountWithBalance> getAccounts() {
+        return accounts;
     }
 }
