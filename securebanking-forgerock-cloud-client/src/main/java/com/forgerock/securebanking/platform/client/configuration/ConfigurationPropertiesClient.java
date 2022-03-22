2ea5478105b743f7e759c928157cbed6f2be2bf9
Jorge Sanchez Perez <54277573+jorgesanchezperez@users.noreply.github.com>
2022-03-22 13:02:10 +0000

    229: cdk-cdm-fidc same configuration (#50)
    * 229: cdk-cdm-fidc same configuration
    - Updated the deployment template with env values from configMap `platform-config`
    - Update the github actions to use the correct version of gcloud configuration environment
    - Updated properties names to normalize them
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/229
    
    * Update rs_server property to rs_fqdn
    
    * Update rsServer field to rsFqdn

--- securebanking-forgerock-cloud-client/src/main/java/com/forgerock/securebanking/platform/client/configuration/ConfigurationPropertiesClient.java
+++ securebanking-forgerock-cloud-client/src/main/java/com/forgerock/securebanking/platform/client/configuration/ConfigurationPropertiesClient.java
@@ -26,8 +26,8 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "identity-platform.client")
 @Data
 public class ConfigurationPropertiesClient {
-    private String igServer;
-    private String iamServer;
+    private String igFqdn;
+    private String identityPlatformFqdn;
     /*
      * Spring maps the properties, the keys from file will be the map keys
      * @see: application-test.yml
--- securebanking-forgerock-cloud-client/src/main/java/com/forgerock/securebanking/platform/client/services/AccountConsentService.java
+++ securebanking-forgerock-cloud-client/src/main/java/com/forgerock/securebanking/platform/client/services/AccountConsentService.java
@@ -92,7 +92,7 @@ public class AccountConsentService implements ConsentService {
     }
 
     private AccountConsentDetails request(String intentId, HttpMethod httpMethod, HttpEntity httpEntity) throws ExceptionClient {
-        String consentURL = configurationProperties.getIgServer() +
+        String consentURL = configurationProperties.getIgFqdn() +
                 UrlContext.replaceParameterContextIntentId(
                         configurationProperties.getContextsAccountsConsent().get(GET.name()),
                         intentId
