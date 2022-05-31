aabc442419bdf2c936bbc22eab10d1ffa387d864
Andra <andra.racovita@itsmartsystems.eu>
2022-05-31 14:36:17 +0300

    282: Add tests for domestic scheduled payment consent decision
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/282

--- securebanking-openbanking-uk-rcs-server/src/test/java/com/forgerock/securebanking/openbanking/uk/rcs/api/ConsentDecisionApiControllerTest.java
+++ securebanking-openbanking-uk-rcs-server/src/test/java/com/forgerock/securebanking/openbanking/uk/rcs/api/ConsentDecisionApiControllerTest.java
@@ -48,6 +48,7 @@ import static com.forgerock.securebanking.platform.client.test.support.AccountAc
 import static com.forgerock.securebanking.platform.client.test.support.ConsentDecisionTestDataFactory.aValidAccountConsentDecision;
 import static com.forgerock.securebanking.platform.client.test.support.ConsentDecisionTestDataFactory.aValidDomesticPaymentConsentDecision;
 import static com.forgerock.securebanking.platform.client.test.support.DomesticPaymentAccessConsentDetailsTestFactory.aValidDomesticPaymentConsentDetails;
+import static com.forgerock.securebanking.platform.client.test.support.DomesticScheduledPaymentAccessConsentDetailsTestFactory.aValidDomesticScheduledPaymentConsentDetails;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -147,6 +148,45 @@ public class ConsentDecisionApiControllerTest {
 
         ConsentDecisionRequest consentDecisionRequest = ConsentDecisionRequest.builder()
                 .accountIds(convert(domesticPaymentConsentDetails.getAsJsonArray("accountsIds")))
+                .consentJwt(jwt)
+                .decision(Constants.ConsentDecision.AUTHORISED)
+                .debtorAccount(financialAccount)
+                .build();
+        HttpEntity<String> request = new HttpEntity(consentDecisionRequest, headers());
+
+        // when
+        ResponseEntity<RedirectionAction> response = restTemplate.postForEntity(consentDetailURL, request, RedirectionAction.class);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getBody().getConsentJwt()).isNotEmpty();
+        assertThat(response.getBody().getRedirectUri()).isNotEmpty();
+    }
+
+    @Test
+    public void ShouldGetRedirectionActionDomesticScheduledPayments() throws ExceptionClient {
+        // given
+        ConsentDecision consentDecision = aValidDomesticPaymentConsentDecision();
+        JsonObject domesticScheduledPaymentConsentDetails = aValidDomesticScheduledPaymentConsentDetails(consentDecision.getIntentId());
+        given(consentServiceClient.updateConsent(consentDecision)).willReturn(domesticScheduledPaymentConsentDetails);
+        String jwt = JwtTestHelper.consentRequestJwt(
+                consentDecision.getClientId(),
+                consentDecision.getIntentId(),
+                consentDecision.getResourceOwnerUsername()
+        );
+
+        given(jwkServiceClient.signClaims(any(JWTClaimsSet.class), anyString())).willReturn(jwt);
+        String consentDetailURL = BASE_URL + port + CONTEXT_DETAILS_URI;
+
+        FRAccountIdentifier accountIdentifier = new FRAccountIdentifier();
+        accountIdentifier.setIdentification("76064512389965");
+        accountIdentifier.setName("John");
+        accountIdentifier.setSchemeName("UK.OBIE.SortCodeAccountNumber");
+        FRFinancialAccount financialAccount = new FRFinancialAccount();
+        financialAccount.setAccounts(List.of(accountIdentifier));
+
+        ConsentDecisionRequest consentDecisionRequest = ConsentDecisionRequest.builder()
+                .accountIds(convert(domesticScheduledPaymentConsentDetails.getAsJsonArray("accountsIds")))
                 .consentJwt(jwt)
                 .decision(Constants.ConsentDecision.AUTHORISED)
                 .debtorAccount(financialAccount)
