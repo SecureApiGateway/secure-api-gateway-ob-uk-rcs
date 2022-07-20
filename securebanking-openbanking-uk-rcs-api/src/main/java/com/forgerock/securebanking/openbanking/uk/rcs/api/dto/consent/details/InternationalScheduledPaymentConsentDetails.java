2ca89f7a877ceaa76976938f2f53f5e8aa1d5bc2
Andra <andra.racovita@itsmartsystems.eu>
2022-07-20 18:02:09 +0300

    404: International scheduled payment consent details
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/404

--- securebanking-openbanking-uk-rcs-api/src/main/java/com/forgerock/securebanking/openbanking/uk/rcs/api/dto/consent/details/InternationalScheduledPaymentConsentDetails.java
+++ securebanking-openbanking-uk-rcs-api/src/main/java/com/forgerock/securebanking/openbanking/uk/rcs/api/dto/consent/details/InternationalScheduledPaymentConsentDetails.java
@@ -15,14 +15,24 @@
  */
 package com.forgerock.securebanking.openbanking.uk.rcs.api.dto.consent.details;
 
+import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.account.FRAccountWithBalance;
 import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.account.FRScheduledPaymentData;
+import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.common.FRAmount;
 import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.common.FRExchangeRateInformation;
 import com.forgerock.securebanking.platform.client.IntentType;
+import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static com.forgerock.securebanking.openbanking.uk.rcs.converters.InternationalPaymentConsentDetailsConverter.DATE_TIME_FORMATTER;
+import static com.forgerock.securebanking.openbanking.uk.rcs.converters.UtilConverter.isNotNull;
+import static com.forgerock.securebanking.platform.client.services.ConsentServiceInterface.log;
 
 /**
  * Models the consent data for an international scheduled payment.
@@ -32,17 +42,48 @@ import org.joda.time.DateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class InternationalScheduledPaymentConsentDetails extends ConsentDetails {
-
-    private FRScheduledPaymentData scheduledPayment;
-    private FRExchangeRateInformation rate;
+    private FRAmount instructedAmount;
+    private FRExchangeRateInformation exchangeRateInformation;
     private String merchantName;
-    private DateTime expiredDate;
+    private DateTime paymentDate;
     private String currencyOfTransfer;
     private String paymentReference;
+    private List<FRAccountWithBalance> accounts;
+
 
     @Override
     public IntentType getIntentType() {
         return IntentType.PAYMENT_INTERNATIONAL_SCHEDULED_CONSENT;
     }
 
+    public void setInstructedAmount(JsonObject instructedAmount) {
+        if (!isNotNull(instructedAmount))
+            this.instructedAmount = null;
+        else {
+            this.instructedAmount = new FRAmount();
+            this.instructedAmount.setAmount(isNotNull(instructedAmount.get("Amount")) ? instructedAmount.get("Amount").getAsString() : null);
+            this.instructedAmount.setCurrency(isNotNull(instructedAmount.get("Currency")) ? instructedAmount.get("Currency").getAsString() : null);
+        }
+    }
+
+    public void setExchangeRateInformation(JsonObject exchangeRateInformation) {
+        if (!isNotNull(exchangeRateInformation))
+            this.exchangeRateInformation = null;
+        else {
+            this.exchangeRateInformation = new FRExchangeRateInformation();
+            this.exchangeRateInformation.setUnitCurrency(isNotNull(exchangeRateInformation.get("UnitCurrency")) ? exchangeRateInformation.get("UnitCurrency").getAsString() : null);
+            String exchangeRate = isNotNull(exchangeRateInformation.get("ExchangeRate")) ? exchangeRateInformation.get("ExchangeRate").getAsString() : null;
+            if (isNotNull(exchangeRate)) {
+                try {
+                    BigDecimal exchangeRateBigDecimal = new BigDecimal(exchangeRate);
+                    this.exchangeRateInformation.setExchangeRate(exchangeRateBigDecimal);
+                } catch (NumberFormatException e) {
+                    log.error("(InternationalScheduledPaymentConsentDetails) the exchange rate couldn't be set");
+                }
+            }
+            this.exchangeRateInformation.setRateType(isNotNull(exchangeRateInformation.get("RateType")) ? FRExchangeRateInformation.FRRateType.fromValue(exchangeRateInformation.get("RateType").getAsString()) : null);
+            this.exchangeRateInformation.setContractIdentification(isNotNull(exchangeRateInformation.get("ContractIdentification")) ? exchangeRateInformation.get("ContractIdentification").getAsString() : null);
+            this.exchangeRateInformation.setExpirationDateTime(isNotNull(exchangeRateInformation.get("ExpirationDateTime")) ? DATE_TIME_FORMATTER.parseDateTime(exchangeRateInformation.get("ExpirationDateTime").getAsString()) : null);
+        }
+    }
 }
