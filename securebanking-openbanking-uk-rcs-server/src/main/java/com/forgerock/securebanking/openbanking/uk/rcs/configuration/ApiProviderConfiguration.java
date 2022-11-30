700e3fe1d4ebcf02c4e12e98833da349c10defa4
jorgesanchezperez <jorge.sanchez-perez@forgerock.com>
2022-11-30 08:39:16 +0000

    Added id api provider configuration, deleted UtilConverter4Test

--- securebanking-openbanking-uk-rcs-server/src/main/java/com/forgerock/securebanking/openbanking/uk/rcs/configuration/ApiProviderConfiguration.java
+++ securebanking-openbanking-uk-rcs-server/src/main/java/com/forgerock/securebanking/openbanking/uk/rcs/configuration/ApiProviderConfiguration.java
@@ -24,6 +24,7 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "api.provider")
 @Data
 public class ApiProviderConfiguration {
+    private String id;
     @Value("${name:Forgerock Bank Simulation}")
     private String name;
     private String address;
