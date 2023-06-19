cff6d145c3463e998fe1856c1de7306da7154ccc
Dan Badham <dan.badham@forgerock.com>
2023-06-19 15:01:04 +0100

    1010: Adding tests for ConsentStoreDetailsService.computeTotalChargeAmount
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1010

--- secure-api-gateway-ob-uk-rcs-server/src/test/java/com/forgerock/sapi/gateway/ob/uk/rcs/server/api/ConsentStoreDetailsServiceTest.java
+++ secure-api-gateway-ob-uk-rcs-server/src/test/java/com/forgerock/sapi/gateway/ob/uk/rcs/server/api/ConsentStoreDetailsServiceTest.java
@@ -25,9 +25,14 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verifyNoInteractions;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -49,6 +54,9 @@ import com.forgerock.sapi.gateway.rcs.consent.store.repo.ConsentStoreEnabledInte
 import com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.payment.DomesticPaymentConsentEntity;
 import com.forgerock.sapi.gateway.rcs.consent.store.repo.service.payment.DomesticPaymentConsentService;
 import com.forgerock.sapi.gateway.uk.common.shared.api.meta.share.IntentType;
+
+import uk.org.openbanking.datamodel.common.OBActiveOrHistoricCurrencyAndAmount;
+import uk.org.openbanking.datamodel.payment.OBWriteDomesticConsentResponse5DataCharges;
 
 @ExtendWith(MockitoExtension.class)
 class ConsentStoreDetailsServiceTest {
@@ -142,5 +150,37 @@ class ConsentStoreDetailsServiceTest {
         assertThat(domesticPaymentConsentDetails.getDebtorAccount()).isNull();
 
         verifyNoInteractions(debtorAccountService);
+    }
+
+
+    private static Stream<Arguments> chargeParameters() {
+        return Stream.of(
+                Arguments.of(List.of("0.01", "0.02", "0.2"), "0.23"),
+                Arguments.of(List.of("0"), "0"),
+                Arguments.of(List.of("0.25"), "0.25")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("chargeParameters")
+    void testCalculateCharges(List<String> chargeAmounts, String expectedTotal) {
+        final List<OBWriteDomesticConsentResponse5DataCharges> charges = chargeAmounts.stream().map(charge -> {
+            final OBWriteDomesticConsentResponse5DataCharges obCharge = new OBWriteDomesticConsentResponse5DataCharges();
+            obCharge.setAmount(new OBActiveOrHistoricCurrencyAndAmount().amount(charge).currency("GBP"));
+            return obCharge;
+        }).collect(Collectors.toList());
+
+        assertThat(ConsentStoreDetailsService.computeTotalChargeAmount(charges)).isEqualTo(new FRAmount(expectedTotal, "GBP"));
+    }
+
+    @Test
+    void testCalculateChargesMismatchCcy() {
+        final List<OBWriteDomesticConsentResponse5DataCharges> charges = List.of(
+                new OBWriteDomesticConsentResponse5DataCharges().amount(new OBActiveOrHistoricCurrencyAndAmount().amount("0.01").currency("GBP")),
+                new OBWriteDomesticConsentResponse5DataCharges().amount(new OBActiveOrHistoricCurrencyAndAmount().amount("0.2").currency("EUR"))
+        );
+
+        final IllegalStateException ex = assertThrows(IllegalStateException.class, () -> ConsentStoreDetailsService.computeTotalChargeAmount(charges));
+        assertThat(ex.getMessage()).isEqualTo("Charges contain more than 1 currency, all charges must be in the same currency");
     }
 }
