b98f5d987f07bc3b7217b41341bff956383719b8
Dan Badham <dan.badham@forgerock.com>
2023-06-26 14:14:01 +0100

    1029: Adding InternationalScheduledPaymentConsentEntity
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1029

---
+++ secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-repo/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/repo/entity/payment/international/InternationalScheduledPaymentConsentEntity.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.payment.international;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.validation.annotation.Validated;
+
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.payment.FRWriteInternationalScheduledConsent;
+
+/**
+ * OBIE International Scheduled Payment Consent: https://openbankinguk.github.io/read-write-api-site3/v3.1.10/resources-and-data-models/pisp/international-scheduled-payment-consents.html
+ */
+@Document("InternationalScheduledPaymentConsent")
+@Validated
+public class InternationalScheduledPaymentConsentEntity extends BaseInternationalPaymentConsentEntity<FRWriteInternationalScheduledConsent> {
+}
