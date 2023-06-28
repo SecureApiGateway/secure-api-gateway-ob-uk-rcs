e780bfb3d0e833e4cc7f5703c5fd32d333290b6a
Dan Badham <dan.badham@forgerock.com>
2023-06-28 13:42:33 +0100

    1032: Adding DomesticVRPConsentEntity + Service
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1032

---
+++ secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-repo/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/repo/entity/payment/vrp/DomesticVRPConsentEntity.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.payment.vrp;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.validation.annotation.Validated;
+
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.vrp.FRDomesticVRPConsent;
+import com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.payment.BasePaymentConsentEntity;
+
+/**
+ * OBIE Domestic VRP Consent: https://openbankinguk.github.io/read-write-api-site3/v3.1.10/resources-and-data-models/vrp/domestic-vrp-consents.html
+ */
+@Document("DomesticVRPConsent")
+@Validated
+public class DomesticVRPConsentEntity extends BasePaymentConsentEntity<FRDomesticVRPConsent> {
+}
