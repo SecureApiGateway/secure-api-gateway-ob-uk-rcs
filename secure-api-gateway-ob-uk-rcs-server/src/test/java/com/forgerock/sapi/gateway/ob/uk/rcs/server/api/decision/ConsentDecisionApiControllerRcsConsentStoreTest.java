9ab4aeb763020380410e2682c7f9163cf937d228
Dan Badham <dan.badham@forgerock.com>
2023-06-29 14:22:42 +0100

    1032: Adding DomesticVRPayment test to ConsentDecisionApiControllerRcsConsentStoreTest
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1032

--- secure-api-gateway-ob-uk-rcs-server/src/test/java/com/forgerock/sapi/gateway/ob/uk/rcs/server/api/decision/ConsentDecisionApiControllerRcsConsentStoreTest.java
+++ secure-api-gateway-ob-uk-rcs-server/src/test/java/com/forgerock/sapi/gateway/ob/uk/rcs/server/api/decision/ConsentDecisionApiControllerRcsConsentStoreTest.java
@@ -20,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -28,11 +29,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.ParseException;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -44,6 +51,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.account.FRFinancialAccount;
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.converter.payment.FRWriteDomesticConsentConverter;
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.converter.vrp.FRDomesticVRPConsentConverters;
 import com.forgerock.sapi.gateway.ob.uk.rcs.api.dto.RedirectionAction;
 import com.forgerock.sapi.gateway.ob.uk.rcs.api.dto.consent.decision.ConsentDecisionDeserialized;
 import com.forgerock.sapi.gateway.ob.uk.rcs.cloud.client.Constants;
@@ -51,9 +61,15 @@ import com.forgerock.sapi.gateway.ob.uk.rcs.server.RCSServerApplicationTestSuppo
 import com.forgerock.sapi.gateway.ob.uk.rcs.server.testsupport.JwtTestHelper;
 import com.forgerock.sapi.gateway.rcs.consent.store.repo.ConsentStoreEnabledIntentTypes;
 import com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.account.AccountAccessConsentEntity;
+import com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.payment.BasePaymentConsentEntity;
+import com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.payment.domestic.DomesticPaymentConsentEntity;
+import com.forgerock.sapi.gateway.rcs.consent.store.repo.entity.payment.vrp.DomesticVRPConsentEntity;
 import com.forgerock.sapi.gateway.rcs.consent.store.repo.service.account.AccountAccessConsentService;
 import com.forgerock.sapi.gateway.rcs.consent.store.repo.service.account.AccountAccessConsentStateModel;
+import com.forgerock.sapi.gateway.rcs.consent.store.repo.service.payment.PaymentConsentService;
+import com.forgerock.sapi.gateway.rcs.consent.store.repo.service.payment.PaymentConsentStateModel;
 import com.forgerock.sapi.gateway.rcs.consent.store.repo.service.payment.domestic.DomesticPaymentConsentService;
+import com.forgerock.sapi.gateway.rcs.consent.store.repo.service.payment.vrp.DomesticVRPConsentService;
 import com.forgerock.sapi.gateway.uk.common.shared.api.meta.obie.OBVersion;
 import com.forgerock.sapi.gateway.uk.common.shared.api.meta.share.IntentType;
 import com.nimbusds.jose.JOSEException;
@@ -68,6 +84,8 @@ import uk.org.openbanking.datamodel.account.OBExternalPermissions1Code;
 import uk.org.openbanking.datamodel.account.OBReadConsent1;
 import uk.org.openbanking.datamodel.account.OBReadData1;
 import uk.org.openbanking.datamodel.account.OBRisk2;
+import uk.org.openbanking.testsupport.payment.OBWriteDomesticConsentTestDataFactory;
+import uk.org.openbanking.testsupport.vrp.OBDomesticVrpConsentRequestTestDataFactory;
 
 /**
  * Spring Boot Test for {@link ConsentDecisionApiController} using the RCS Consent Store.
@@ -82,6 +100,8 @@ public class ConsentDecisionApiControllerRcsConsentStoreTest {
     private static final String TEST_RESOURCE_OWNER_ID = "test-resource-owner-1";
 
     private static final List<String> TEST_ACCOUNT_ACCESS_ACCOUNT_IDS = List.of("acc-1", "acc-2");
+
+    private static final String TEST_PAYMENT_DEBTOR_ACC_ID = "debtor-acc-1";
 
     @LocalServerPort
     private int port;
@@ -101,6 +121,9 @@ public class ConsentDecisionApiControllerRcsConsentStoreTest {
 
     @Autowired
     private DomesticPaymentConsentService domesticPaymentConsentService;
+
+    @Autowired
+    private DomesticVRPConsentService domesticVRPConsentService;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -157,6 +180,88 @@ public class ConsentDecisionApiControllerRcsConsentStoreTest {
         assertEquals(TEST_RESOURCE_OWNER_ID, authorisedConsent.getResourceOwnerId());
         assertEquals(TEST_ACCOUNT_ACCESS_ACCOUNT_IDS, authorisedConsent.getAuthorisedAccountIds());
     }
+
+    private static Stream<Arguments> paymentConsentArguments() {
+        return Stream.of(
+                arguments(IntentType.PAYMENT_DOMESTIC_CONSENT)
+        );
+    }
+
+    private BasePaymentConsentEntity createPaymentConsent(IntentType intentType) {
+        switch (intentType) {
+        case PAYMENT_DOMESTIC_CONSENT:
+            return createDomesticPaymentConsentEntity();
+        case DOMESTIC_VRP_PAYMENT_CONSENT:
+            return createDomesticVRPConsentEntity();
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    private PaymentConsentService getConsentService(IntentType intentType) {
+        switch (intentType) {
+        case PAYMENT_DOMESTIC_CONSENT:
+            return domesticPaymentConsentService;
+        case DOMESTIC_VRP_PAYMENT_CONSENT:
+            return domesticVRPConsentService;
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("paymentConsentArguments")
+    public void testAuthorisePaymentConsent(IntentType intentType) {
+        Assumptions.assumeTrue(consentStoreEnabledIntentTypes.isIntentTypeSupported(intentType));
+
+        final BasePaymentConsentEntity consent = createPaymentConsent(intentType);
+        final String consentRequestJwt = JwtTestHelper.consentRequestJwt(TEST_API_CLIENT_ID, consent.getId(), TEST_RESOURCE_OWNER_ID);
+
+        final FRFinancialAccount debtorAccount = new FRFinancialAccount();
+        debtorAccount.setAccountId(TEST_PAYMENT_DEBTOR_ACC_ID);
+        final ConsentDecisionDeserialized consentDecisionDeserialized = ConsentDecisionDeserialized.builder()
+                .debtorAccount(debtorAccount)
+                .consentJwt(consentRequestJwt)
+                .decision(Constants.ConsentDecisionStatus.AUTHORISED)
+                .build();
+        final HttpEntity<ConsentDecisionDeserialized> request = new HttpEntity<>(consentDecisionDeserialized, headers());
+
+        final ResponseEntity<RedirectionAction> response = restTemplate.postForEntity(consentDecisionUrl, request, RedirectionAction.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getBody()).isNotNull();
+        final String consentResponseJwt = response.getBody().getConsentJwt();
+        assertThat(consentResponseJwt).isNotEmpty();
+        verifyConsentResponseJwt(consentResponseJwt);
+
+        // Verify consent in store is now authorised
+        final PaymentConsentService consentService = getConsentService(intentType);
+        final BasePaymentConsentEntity authorisedConsent = (BasePaymentConsentEntity) consentService.getConsent(consent.getId(), consent.getApiClientId());
+        assertEquals(PaymentConsentStateModel.AUTHORISED, authorisedConsent.getStatus());
+        assertEquals(TEST_RESOURCE_OWNER_ID, authorisedConsent.getResourceOwnerId());
+        assertEquals(TEST_PAYMENT_DEBTOR_ACC_ID, authorisedConsent.getAuthorisedDebtorAccountId());
+    }
+
+    DomesticPaymentConsentEntity createDomesticPaymentConsentEntity() {
+        final DomesticPaymentConsentEntity consent = new DomesticPaymentConsentEntity();
+        consent.setApiClientId(TEST_API_CLIENT_ID);
+        consent.setRequestVersion(OBVersion.v3_1_10);
+        consent.setStatus(PaymentConsentStateModel.AWAITING_AUTHORISATION);
+        consent.setRequestObj(FRWriteDomesticConsentConverter.toFRWriteDomesticConsent(OBWriteDomesticConsentTestDataFactory.aValidOBWriteDomesticConsent4()));
+        consent.setIdempotencyKey(UUID.randomUUID().toString());
+        consent.setIdempotencyKeyExpiration(DateTime.now().plusDays(1));
+        return domesticPaymentConsentService.createConsent(consent);
+    }
+
+    DomesticVRPConsentEntity createDomesticVRPConsentEntity() {
+        final DomesticVRPConsentEntity consent = new DomesticVRPConsentEntity();
+        consent.setApiClientId(TEST_API_CLIENT_ID);
+        consent.setRequestVersion(OBVersion.v3_1_10);
+        consent.setStatus(PaymentConsentStateModel.AWAITING_AUTHORISATION);
+        consent.setRequestObj(FRDomesticVRPConsentConverters.toFRDomesticVRPConsent(OBDomesticVrpConsentRequestTestDataFactory.aValidOBDomesticVRPConsentRequest()));
+        consent.setIdempotencyKey(UUID.randomUUID().toString());
+        consent.setIdempotencyKeyExpiration(DateTime.now().plusDays(1));
+        return domesticVRPConsentService.createConsent(consent);
+    }
+
 
     private HttpHeaders headers() {
         HttpHeaders headers = new HttpHeaders();
