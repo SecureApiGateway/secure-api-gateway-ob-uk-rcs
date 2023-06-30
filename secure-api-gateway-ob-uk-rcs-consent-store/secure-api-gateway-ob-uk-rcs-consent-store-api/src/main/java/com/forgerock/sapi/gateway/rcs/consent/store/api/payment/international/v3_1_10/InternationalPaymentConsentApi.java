75de11a1ebeb7aeb84fccdf70ad6df523b449c9d
Dan Badham <dan.badham@forgerock.com>
2023-06-30 10:07:25 +0100

    1032: Removing x-api-client-id header from POST endpoints for InternationalPaymentConsentApi
    x-api-client-id header is only needed for GET requests, for POSTs the value is in the request body
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1032

--- secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-api/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/api/payment/international/v3_1_10/InternationalPaymentConsentApi.java
+++ secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-api/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/api/payment/international/v3_1_10/InternationalPaymentConsentApi.java
@@ -58,11 +58,9 @@ public interface InternationalPaymentConsentApi {
             consumes = {"application/json; charset=utf-8"},
             produces = {"application/json; charset=utf-8"},
             method = RequestMethod.POST)
-    ResponseEntity<InternationalPaymentConsent> createConsent(
-            @ApiParam(value = "Create Consent Request", required = true)
-            @Valid
-            @RequestBody CreateInternationalPaymentConsentRequest request,
-            @RequestHeader(value = "x-api-client-id") String apiClientId);
+    ResponseEntity<InternationalPaymentConsent> createConsent(@ApiParam(value = "Create Consent Request", required = true)
+                                                              @Valid
+                                                              @RequestBody CreateInternationalPaymentConsentRequest request);
 
 
     @ApiOperation(value = "Get International Payment Consent")
@@ -101,8 +99,7 @@ public interface InternationalPaymentConsentApi {
     ResponseEntity<InternationalPaymentConsent> authoriseConsent(@PathVariable(value = "consentId") String consentId,
                                                                  @ApiParam(value = "Authorise Consent Request", required = true)
                                                                  @Valid
-                                                                 @RequestBody AuthorisePaymentConsentRequest request,
-                                                                 @RequestHeader(value = "x-api-client-id") String apiClientId);
+                                                                 @RequestBody AuthorisePaymentConsentRequest request);
 
 
     @ApiOperation(value = "Reject International Payment Consent")
@@ -123,8 +120,7 @@ public interface InternationalPaymentConsentApi {
     ResponseEntity<InternationalPaymentConsent> rejectConsent(@PathVariable(value = "consentId") String consentId,
                                                               @ApiParam(value = "Reject Consent Request", required = true)
                                                               @Valid
-                                                              @RequestBody RejectConsentRequest request,
-                                                              @RequestHeader(value = "x-api-client-id") String apiClientId);
+                                                              @RequestBody RejectConsentRequest request);
 
 
 
@@ -146,7 +142,6 @@ public interface InternationalPaymentConsentApi {
     ResponseEntity<InternationalPaymentConsent> consumeConsent(@PathVariable(value = "consentId") String consentId,
                                                                @ApiParam(value = "Consume Consent Request", required = true)
                                                                @Valid
-                                                               @RequestBody ConsumePaymentConsentRequest request,
-                                                               @RequestHeader(value = "x-api-client-id") String apiClientId);
+                                                               @RequestBody ConsumePaymentConsentRequest request);
 
 }
--- secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-api/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/api/payment/international/v3_1_10/InternationalPaymentConsentApiController.java
+++ secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-api/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/api/payment/international/v3_1_10/InternationalPaymentConsentApiController.java
@@ -70,8 +70,8 @@ public class InternationalPaymentConsentApiController implements InternationalPa
     }
 
     @Override
-    public ResponseEntity<InternationalPaymentConsent> createConsent(CreateInternationalPaymentConsentRequest request, String apiClientId) {
-        logger.info("Attempting to createConsent: {}, for apiClientId: {}", request, apiClientId);
+    public ResponseEntity<InternationalPaymentConsent> createConsent(CreateInternationalPaymentConsentRequest request) {
+        logger.info("Attempting to createConsent: {}", request);
         final InternationalPaymentConsentEntity internationalPaymentConsent = new InternationalPaymentConsentEntity();
         internationalPaymentConsent.setRequestVersion(obVersion);
         internationalPaymentConsent.setApiClientId(request.getApiClientId());
@@ -94,23 +94,23 @@ public class InternationalPaymentConsentApiController implements InternationalPa
     }
 
     @Override
-    public ResponseEntity<InternationalPaymentConsent> authoriseConsent(String consentId, AuthorisePaymentConsentRequest request, String apiClientId) {
-        logger.info("Attempting to authoriseConsent - id: {}, request: {}, apiClientId: {}", consentId, request, apiClientId);
-        final PaymentAuthoriseConsentArgs paymentAuthoriseConsentArgs = new PaymentAuthoriseConsentArgs(consentId, apiClientId,
-                                                                                                                                request.getResourceOwnerId(), request.getAuthorisedDebtorAccountId());
+    public ResponseEntity<InternationalPaymentConsent> authoriseConsent(String consentId, AuthorisePaymentConsentRequest request) {
+        logger.info("Attempting to authoriseConsent - id: {}, request: {}", consentId, request);
+        final PaymentAuthoriseConsentArgs paymentAuthoriseConsentArgs = new PaymentAuthoriseConsentArgs(consentId,
+                request.getApiClientId(), request.getResourceOwnerId(), request.getAuthorisedDebtorAccountId());
         return ResponseEntity.ok(convertEntityToDto(consentService.authoriseConsent(paymentAuthoriseConsentArgs)));
     }
 
     @Override
-    public ResponseEntity<InternationalPaymentConsent> rejectConsent(String consentId, RejectConsentRequest request, String apiClientId) {
-        logger.info("Attempting to rejectConsent - id: {}, request: {}, apiClientId: {}", consentId, request, apiClientId);
-        return ResponseEntity.ok(convertEntityToDto(consentService.rejectConsent(consentId, apiClientId, request.getResourceOwnerId())));
+    public ResponseEntity<InternationalPaymentConsent> rejectConsent(String consentId, RejectConsentRequest request) {
+        logger.info("Attempting to rejectConsent - id: {}, request: {}", consentId, request);
+        return ResponseEntity.ok(convertEntityToDto(consentService.rejectConsent(consentId, request.getApiClientId(), request.getResourceOwnerId())));
     }
 
     @Override
-    public ResponseEntity<InternationalPaymentConsent> consumeConsent(String consentId, ConsumePaymentConsentRequest request, String apiClientId) {
-        logger.info("Attempting to consumeConsent - id: {}, request: {}, apiClientId: {}", consentId, request, apiClientId);
-        return ResponseEntity.ok(convertEntityToDto(consentService.consumeConsent(consentId, apiClientId)));
+    public ResponseEntity<InternationalPaymentConsent> consumeConsent(String consentId, ConsumePaymentConsentRequest request) {
+        logger.info("Attempting to consumeConsent - id: {}, request: {}", consentId, request);
+        return ResponseEntity.ok(convertEntityToDto(consentService.consumeConsent(consentId, request.getApiClientId())));
     }
 
     private InternationalPaymentConsent convertEntityToDto(InternationalPaymentConsentEntity entity) {
