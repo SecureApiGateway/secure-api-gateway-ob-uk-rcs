be09e3b6bacd85631ab3719bd517a0c14a2cd891
Laura Jianu <108405474+frlaura-jianu@users.noreply.github.com>
2024-11-05 17:15:02 +0200

    1553: Support OBIE Read/Write API v4.0.0 (#259)
    * 1553: Adding RCS support
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1553
    
    * 1553: Fixing v3.1.10 consentStoreClient
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1553

--- secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-client/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/client/payment/domesticscheduled/v4_0_0/RestDomesticScheduledPaymentConsentStoreClient.java
+++ secure-api-gateway-ob-uk-rcs-consent-store/secure-api-gateway-ob-uk-rcs-consent-store-client/src/main/java/com/forgerock/sapi/gateway/rcs/consent/store/client/payment/domesticscheduled/v4_0_0/RestDomesticScheduledPaymentConsentStoreClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.forgerock.sapi.gateway.rcs.consent.store.client.payment.domesticscheduled.v3_1_10;
+package com.forgerock.sapi.gateway.rcs.consent.store.client.payment.domesticscheduled.v4_0_0;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -24,16 +24,15 @@ import com.forgerock.sapi.gateway.rcs.consent.store.client.payment.domesticsched
 import com.forgerock.sapi.gateway.uk.common.shared.api.meta.obie.OBVersion;
 
 /**
- * v3.1.10 implementation of RestDomesticScheduledPaymentConsentStoreClient
+ * v4.0.0 implementation of RestDomesticScheduledPaymentConsentStoreClient
  */
-@Component("v3.1.10RestDomesticScheduledPaymentConsentStoreClient")
+@Component("v4.0.0RestDomesticScheduledPaymentConsentStoreClient")
 public class RestDomesticScheduledPaymentConsentStoreClient extends BaseRestDomesticScheduledPaymentConsentStoreClient {
 
-    public RestDomesticScheduledPaymentConsentStoreClient(
-            ConsentStoreClientConfiguration consentStoreClientConfiguration,
+    public RestDomesticScheduledPaymentConsentStoreClient(ConsentStoreClientConfiguration consentStoreClientConfiguration,
             RestTemplateBuilder restTemplateBuilder,
             ObjectMapper objectMapper) {
 
-        super(consentStoreClientConfiguration, restTemplateBuilder, objectMapper, OBVersion.v3_1_10);
+        super(consentStoreClientConfiguration, restTemplateBuilder, objectMapper, OBVersion.v4_0_0);
     }
 }
